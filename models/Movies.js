891f73cfd0996061df3047e08ff6b5791e66e650
charity.rogers <rogerscl116@gmail.com>
2021-07-25 13:50:02 -0500

    save Movies model

---
+++ models/Movies.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Movies extends Model {}
+
+Movies.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+      genre_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'movies',
+  }
+);
+
+module.exports = Movies;
