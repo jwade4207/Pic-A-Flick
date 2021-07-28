a4e3a933831ba5a047ce1f6b4d0e4f011e5b6aab
charity.rogers <rogerscl116@gmail.com>
2021-07-27 20:37:51 -0500

    move javascript files

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,34 +1,7 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-      
-      // check the response status
-      if (response.ok) {
-          console.log('success');
-      } else {
-          alert(response.statusText);
-      }
-    }
-};
-
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const email = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
@@ -42,12 +15,12 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
-        alert(response.statusText);
+        let result = await response.json()
+        alert(result.message)
       }
     }
-}
+  }
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
