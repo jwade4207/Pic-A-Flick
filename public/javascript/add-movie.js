b12ac124bc03b4276cdc7744f6b828bc04f0aaed
charity.rogers <rogerscl116@gmail.com>
2021-07-29 21:50:34 -0500

    add user-info partial and edit dashboard routes

--- public/javascript/add-movie.js
+++ public/javascript/add-movie.js
@@ -3,7 +3,7 @@ async function newFormHandler(event) {
 
     // get the movie title and genre from the form
     const title = document.querySelector('input[name="movie-title"]').value;
-    const genre_text = document.querySelector('textarea[name="genre-text"]').value;
+    const genre_name = document.querySelector('textarea[name="genre-name"]').value;
 
     const response = await fetch(`/api/movies`, {
       method: 'POST',
