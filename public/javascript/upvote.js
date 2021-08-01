7f705c0221fe2134ab963ec0be2357f15deed4fb
charity.rogers <rogerscl116@gmail.com>
2021-08-01 18:53:23 -0500

    add procfile

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -4,10 +4,10 @@ async function upvoteClickHandler(event) {
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-    const response = await fetch('/api/posts/upvote', {
+    const response = await fetch('/api/movies/upvote', {
       method: 'PUT',
       body: JSON.stringify({
-        post_id: id
+        movie_id: id
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -21,4 +21,4 @@ async function upvoteClickHandler(event) {
     }
   }
   
-  //document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+  // document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
