b82bf6f6cee42efa8fd6852ab648d4a59482ac80
unknown <michael_pnkstn@yahoo.com>
2021-08-01 19:00:06 -0400

    re-labeling radio btns and finishing btn logic

--- public/javascript/vote.js
+++ public/javascript/vote.js
@@ -1,6 +1,15 @@
 async function voteClickHandler(event) {
     event.preventDefault();
 
+    const radiobtns = document.querySelectorAll('input[name="choice"]');
+    let selectedValue;
+    for (const rb of radiobtns) {
+        if(rb.checked) {
+            selectedValue = rb.Value;
+            break;
+        }
+    }
+
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 // const genresRoutes = require('./genres-routes');
 const moviesRoutes = require('./movies-routes');
-// const voteRoutes = require('./vote-routes');
+const voteRoutes = require('./vote-routes');
 
 router.use('/users', userRoutes);
 // router.use('/genres', genresRoutes);
